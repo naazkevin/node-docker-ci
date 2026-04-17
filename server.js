478f9ce1bb13a5428f0290a390921b93b199e9fa
naazkevin <132082207+naazkevin@users.noreply.github.com>
2026-04-17 15:09:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.end("Hello from DevOps CI/CD 🚀");
+  res.end("Hello from DevOps CI/CD 🚀 v2");
 });
 
 server.listen(3000, () => {
